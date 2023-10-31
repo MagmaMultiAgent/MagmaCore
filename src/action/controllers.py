fix factory placement and action masking

src/action/controllers.py
@@ -120,7 +120,7 @@ class SimpleUnitDiscreteController(Controller):
         transfer_dir = id % (self.transfer_act_dims/3)
         transfer_type_mapping = [0, 1, 4]  # Mapping index to desired value
         transfer_type_index = id // (self.transfer_act_dims/3)  # 0 for ice, 1 for ore, 2 for power
-        transfer_type = transfer_type_mapping[transfer_type_index]
+        transfer_type = transfer_type_mapping[transfer_type_index.astype(int).item()]
         return np.array([1, transfer_dir, transfer_type, self.env_cfg.max_transfer_amount, 0, 1])
 
     def _is_pickup_action(self, id):
@@ -159,10 +159,26 @@ class SimpleUnitDiscreteController(Controller):
         """
         return np.array([4, 0, 0, 0, 0, 1])
 
+    def _get_build_light_action(self):
+        """
+        Converts the action id to build light factory action
+        """
+        return 0
+
     def action_to_lux_action(
         self, agent: str, obs: Dict[str, Any], action: npt.NDArray
     ):
         unit_action = self.unit_action_to_lux_action(agent, obs, action)
+
+        # add build actions for factories
+        shared_obs = obs[agent]
+        factories = shared_obs["factories"][agent]
+        for factory_id, factory in factories.items():
+            self.logger.debug(factory)
+            unit_action[factory_id] = self._get_build_light_action()
+
+        self.logger.debug(f"Final actions: {unit_action}")
+
         return unit_action
 
     def unit_action_to_lux_action(
@@ -171,7 +187,7 @@ class SimpleUnitDiscreteController(Controller):
         """
         Converts the action to a lux action
         """
-        self.logger.debug(f"Creating lux action for agent {agent} with action\n{action}")
+        self.logger.debug(f"Creating lux action for agent {agent}")
 
         shared_obs = obs["player_0"]
         lux_action = {}
@@ -239,14 +255,15 @@ class SimpleUnitDiscreteController(Controller):
             np.ones_like(shared_obs["board"]["rubble"], dtype=int) * -1
         )
         for player in shared_obs["units"]:
+            self.logger.debug(f"Player units: {player} {len(shared_obs['units'][player])}")
             for unit_id in shared_obs["units"][player]:
                 unit = shared_obs["units"][player][unit_id]
                 pos = np.array(unit["pos"])
-                player_occupancy_map[pos[0], pos[1]] = unit["id"]
-                
-        factories = {}
+                self.logger.debug(unit)
+                player_occupancy_map[pos[0], pos[1]] = int(unit["unit_id"].split("_")[1])
+
         for player in shared_obs["factories"]:
-            factories[player] = {}
+            self.logger.debug(f"Player factories: {player} {len(shared_obs['factories'][player])}")
             for unit_id in shared_obs["factories"][player]:
                 f_data = shared_obs["factories"][player][unit_id]
                 f_pos = f_data["pos"]
@@ -258,7 +275,7 @@ class SimpleUnitDiscreteController(Controller):
         units = shared_obs["units"][agent]
         action_mask = np.zeros((self.total_act_dims, self.env_cfg.map_size, self.env_cfg.map_size), dtype=bool)
         for unit_id in units.keys():
-            
+
             # get position of unit
             pos = np.array(unit["pos"])
 
@@ -324,7 +341,7 @@ class SimpleUnitDiscreteController(Controller):
                 ] = False
 
             # recharge should only be valid in the night
-            if shared_obs["step"] % 40 > 30:
+            if shared_obs["real_env_steps"] % 40 > 30:
                 action_mask[
                     self.recharge_dim_high - self.recharge_act_dims : self.recharge_dim_high, pos[0], pos[1]
                 ] = True

src/luxai_s2/utils/heuristics/factory_placement.py
@@ -2,22 +2,30 @@ import numpy as np
 from luxai_s2.unit import FactoryPlacementActionType
 from luxai_s2.state import ObservationStateDict
 
+import logging
+logger = logging.getLogger(__name__)
+
+
 def random_factory_placement(player, obs: ObservationStateDict) -> FactoryPlacementActionType:
     """
     This policy places factories with 150 water and metal at random locations
     """
+
+    logger.debug("Using random_factory_placement policy")
+
     # we will spawn our factory in a random location with 150 metal and water if it is our turn to place
     potential_spawns = np.array(list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1))))
+    logger.debug(f"Potential spawns: {len(potential_spawns)}")
     spawn_loc = potential_spawns[np.random.randint(0, len(potential_spawns))]
     return dict(spawn=spawn_loc, metal=150, water=150)
 
 def place_near_random_ice(player, obs: ObservationStateDict):
-    if obs["teams"][player]["metal"] == 0:
-        return dict()
+    logger.debug("Using place_near_random_ice policy")
+    
     potential_spawns = list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1)))
     potential_spawns_set = set(potential_spawns)
+    logger.debug(f"Potential spawns: {len(potential_spawns)}")
     done_search = False
-    # if player == "player_1":
     ice_diff = np.diff(obs["board"]["ice"])
     pot_ice_spots = np.argwhere(ice_diff == 1)
     if len(pot_ice_spots) == 0:
@@ -44,5 +52,4 @@ def place_near_random_ice(player, obs: ObservationStateDict):
     if not done_search:
         pos = spawn_loc
 
-    metal = obs["teams"][player]["metal"]
-    return dict(spawn=pos, metal=metal, water=metal)
+    return dict(spawn=pos, metal=150, water=150)

src/luxai_s2/wrappers/sb3.py
@@ -64,6 +64,7 @@ class SB3Wrapper(gym.Wrapper):
         # The simplified wrapper removes the first two phases of the game by using predefined policies (trained or heuristic)
         # to handle those two phases during each reset
         if factory_placement_policy is None:
+            self.logger.debug("Factory policy is None")
             def factory_placement_policy(player, obs: ObservationStateDict):
                 potential_spawns = np.array(
                     list(zip(*np.where(obs["board"]["valid_spawns_mask"] == 1)))
@@ -86,7 +87,7 @@ class SB3Wrapper(gym.Wrapper):
         self.prev_obs = None
 
     def step(self, action: Dict[str, npt.NDArray]):
-        self.logger.debug(f"Stepping environment with action\n{action}")
+        self.logger.debug(f"Stepping environment")
 
         # here, for each agent in the game we translate their action into a Lux S2 action
         lux_action = dict()
@@ -111,14 +112,18 @@ class SB3Wrapper(gym.Wrapper):
         obs, _ = self.env.reset(**kwargs)
         
         # then use the bid policy to go through the bidding phase
+        self.logger.debug("Bidding")
         action = dict()
         for agent in self.env.agents:
             action[agent] = self.bid_policy(agent, obs[agent])
+            self.logger.debug(f"bidding for agent {agent} with action {action[agent]}")
         obs, _, _, _, _ = self.env.step(action)
         
         # while real_env_steps < 0, we are in the factory placement phase
         # so we use the factory placement policy to step through this
+        self.logger.debug("Placing factories")
         while self.env.state.real_env_steps < 0:
+            self.logger.debug(f"real env steps: {self.env.state.real_env_steps}")
             action = dict()
             for agent in self.env.agents:
                 if my_turn_to_place_factory(
@@ -126,9 +131,15 @@ class SB3Wrapper(gym.Wrapper):
                     self.env.state.env_steps,
                 ):
                     action[agent] = self.factory_placement_policy(agent, obs[agent])
+                    self.logger.info(f"my turn: {agent} {action[agent]}")
                 else:
                     action[agent] = dict()
+                    self.logger.info(f"not my turn: {agent} {action[agent]}")
+            self.logger.debug(f"placement: {action}")
             obs, _, _, _, _ = self.env.step(action)
         self.prev_obs = obs
+
+        for agent in self.env.agents:
+            assert obs[agent]["teams"][agent]["factories_to_place"] == 0
         
         return obs, {}

src/sb3_contrib/common/maskable/policies.py
@@ -19,6 +19,8 @@ from torch import nn
 
 from sb3_contrib.common.maskable.distributions import MaskableDistribution, make_masked_proba_distribution
 
+import sys
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -148,6 +150,9 @@ class MaskableActorCriticPolicy(BasePolicy):
 
         if action_masks is not None:
             assert latent_shape == action_masks.shape
+
+        #np.set_printoptions(threshold=sys.maxsize)
+        self.logger.debug(f"Action mask: {action_masks.shape}\n{action_masks.astype(int).sum(axis=(1, 2, 3))}")
 
         batch_size, action_channels, height, width = latent_shape
 
@@ -379,7 +384,7 @@ class MaskableActorCriticPolicy(BasePolicy):
         if action_masks is not None:
             distribution.apply_masking(action_masks)
         actions = actions.view(-1)
-        self.logger.debug(f"Transformed saved actions: {actions.shape}")
+        self.logger.debug(f"Transformed saved actions:\n{actions}")
         log_prob = distribution.log_prob(actions)
         
         log_prob = log_prob.view(batch_size, height, width)